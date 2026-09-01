5a416c698c92151a70cee5eae91a12d5a53701a9
Reynaldzxc <202480142@psu.palawan.edu.ph>
2026-09-02 02:34:49 +0800

    CRUD to all models

--- projectsite/studentorg/forms.py
+++ projectsite/studentorg/forms.py
@@ -1,10 +1,36 @@
 
-
 from django.forms import ModelForm
-from .models import Organization
+from .models import Organization, OrgMember, Student, College, Program
 
 
 class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
         fields = "__all__"
+
+
+class OrgMemberForm(ModelForm):
+    class Meta:
+        model = OrgMember
+        fields = ['student', 'organization', 'date_joined']
+
+class StudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = [
+            'student_id',
+            'lastname',
+            'firstname',
+            'middlename',
+            'program'
+        ]
+
+class CollegeForm(ModelForm):
+    class Meta:
+        model = College
+        fields = ['college_name']
+
+class ProgramForm(ModelForm):
+    class Meta:
+        model = Program
+        fields = ['prog_name', 'college']
